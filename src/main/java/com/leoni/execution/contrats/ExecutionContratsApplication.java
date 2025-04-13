e71f6831b16393c1ae6a47e8ed88818900526652
Rayan-Gazzah <rayan.gazzah@horizon-tech.tn>
2025-04-13 22:45:09 +0100

    Initial commit

---
+++ src/main/java/com/leoni/execution/contrats/ExecutionContratsApplication.java
@@ -0,0 +1,13 @@
+package com.leoni.execution.contrats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExecutionContratsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExecutionContratsApplication.class, args);
+	}
+
+}
